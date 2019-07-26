09cee963a683ba796e52f25f2841587d10fddd99
adpaquin <adpaquin@gmail.com>
2019-07-25 20:52:40 -0400

    starting to add graphs

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import AlbumsIndexContainer from '../containers/AlbumsIndexContainer'
 import AlbumShowContainer from '../containers/AlbumShowContainer'
+import GraphShowContainer from '../containers/GraphShowContainer'
 
 export const App = (props) => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/albums" component={AlbumsIndexContainer} />
-        <Route exact path="/albums/:id" component={AlbumShowContainer} />
+        <Route exact path="/albums/:id" component={GraphShowContainer} />
       </Switch>
     </BrowserRouter>
   )
