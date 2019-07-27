df0889759e25b9dded753c8f7b9b3444fe809521
adpaquin <adpaquin@gmail.com>
2019-07-27 10:58:22 -0400

    graphs now use album audio features instead of sample data

--- app/javascript/react/containers/GraphShowContainer.js
+++ app/javascript/react/containers/GraphShowContainer.js
@@ -6,18 +6,7 @@ class GraphShowContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      albumInfo: [],
-      data: [
-        {
-          name: 'Album 1',
-          acousticness: 70,
-          danceability: 100,
-          energy: 80,
-          instrumentalness: 80,
-          liveness: 60,
-          tempo: 70
-        }
-      ]
+      albumInfo_1: [],
     }
   }
 
@@ -35,42 +24,32 @@ class GraphShowContainer extends Component {
       })
       .then(response => response.json())
       .then(body => {
-        this.setState({ albumInfo: body })
+        this.setState({ albumInfo_1: [body] })
       })
       .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
   render() {
-
     const basicFormat = format('.1r');
 
     return (
 
-      <div>
-        <div>
-          Artist: {this.state.albumInfo.artist_name}
-        </div>
-        <div>
-          Album: {this.state.albumInfo.name}
-        </div>
 
       <RadarChart
-        data={this.state.data}
+        data={this.state.albumInfo_1}
         tickFormat={t => basicFormat(t)}
         startingAngle={0}
         domains={[
-          {name: 'acousticness', domain: [0, 100], getValue: d => d.acousticness},
-          {name: 'danceability', domain: [0, 100], getValue: d => d.danceability},
-          {name: 'energy', domain: [0, 100], getValue: d => d.energy},
-          {name: 'instrumentalness', domain: [0, 100], getValue: d => d.instrumentalness},
-          {name: 'liveness', domain: [0, 100], getValue: d => d.liveness},
-          {name: 'tempo', domain: [0, 100], getValue: d => d.tempo}
+          {name: 'acousticness', domain: [0, 100], getValue: d => d.acousticness_average},
+          {name: 'danceability', domain: [0, 100], getValue: d => d.danceability_average},
+          {name: 'energy', domain: [0, 100], getValue: d => d.energy_average},
+          {name: 'instrumentalness', domain: [0, 100], getValue: d => d.instrumentalness_average},
+          {name: 'liveness', domain: [0, 100], getValue: d => d.liveness_average},
+          {name: 'tempo', domain: [0, 1000], getValue: d => d.tempo_average}
         ]}
         width={600}
         height={500}
       />
-
-      </div>
     );
   }
 }
