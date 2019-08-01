c99308afdc01341c2b1507c456eaf5912200bbb9
adpaquin <adpaquin@gmail.com>
2019-08-01 16:43:43 -0400

    updated clickable tiles so the main album is not included

--- app/javascript/react/containers/GraphShowContainer.js
+++ app/javascript/react/containers/GraphShowContainer.js
@@ -12,7 +12,8 @@ class GraphShowContainer extends Component {
     super(props)
     this.state = {
       albumInfo: [],
-      albumTiles: []
+      albumTiles: [],
+      albumBaseId: ''
     }
     this.addAlbum = this.addAlbum.bind(this)
     this.showCompareableAlbums = this.showCompareableAlbums.bind(this)
@@ -79,7 +80,8 @@ class GraphShowContainer extends Component {
       })
       .then(response => response.json())
       .then(body => {
-        this.setState({ albumInfo: [body] })
+        let albumBaseId = body.id
+        this.setState({ albumInfo: [body], albumBaseId: albumBaseId })
       })
       .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
@@ -92,8 +94,14 @@ class GraphShowContainer extends Component {
     let mainAlbumName = ''
     let mainAlbumSongs = []
     let items = []
+    let baseId = this.state.albumBaseId
+    // debugger
 
-    let albumSelectTile = this.state.albumTiles.map(album => {
+    let albumSelectTile = this.state.albumTiles.filter(album => {
+        return(album.id !== baseId)
+    })
+
+      albumSelectTile = albumSelectTile.map(album => {
       return(
         <div id={album.id} onClick={ () => this.addAlbum(album.id) }>
           <img src={album.cover_image} height="200" width="200" />
